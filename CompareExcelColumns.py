f02dc9555f0fb4879fe3fde9d48e244e3d6eefc2
micheal <emtuyie@gmail.com>
2023-11-11 15:29:26 -0500

    xcel input

--- CompareExcelColumns.py
+++ CompareExcelColumns.py
@@ -1,13 +1,16 @@
 import pandas as pd
 
 # Load the two Excel files
-file1_path = 'file1.xlsx'
-file2_path = 'file2.xlsx'
+file1_path = input('Enter file1.xlsx -> ')
+file2_path = input('Enter file2.xlsx -> ')
+
+sheet1 = input('Enter Sheet1 Name -> ')
+sheet2 = input('Enter Sheet2 Name -> ')
 
 # Load the second column from both files
-column_name = 'Client Name'  # Replace with the actual column name
-df1 = pd.read_excel(file1_path, sheet_name='Services Rendered Report Advanc', usecols=[1], names=[column_name])
-df2 = pd.read_excel(file2_path, sheet_name='Services Rendered Report Advanc', usecols=[1], names=[column_name])
+column_name = input('Enter column name ') # ie Client Name/column name
+df1 = pd.read_excel(file1_path, sheet_name= f'{sheet1}', usecols=[1], names=[column_name])
+df2 = pd.read_excel(file2_path, sheet_name= f'{sheet2}', usecols=[1], names=[column_name])
 
 # Find the differences between the columns
 differences = pd.concat([df1, df2]).drop_duplicates(keep=False)
